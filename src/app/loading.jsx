60c35abe2490d9f9a2151a8e23b7bdfa005d9414
ElsayedRapaa <Elsayedrapaa7@gmail.com>
2023-03-23 15:04:14 +0200

    Add Loading Effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+      <img src="Spinner.svg" alt="Loading..." className="h-80" />
+    </div>
+  );
+}
